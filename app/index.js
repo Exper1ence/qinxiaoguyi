5b232bc951bf2296d90dfa19c48c81eaf00eaeef
Exper1ence <3533357503@qq.com>
2017-01-13 14:32:12 +0800

    update

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const Path = require('path');
 
 module.exports = function mathRobot(router) {
     router.static(Path.resolve(__dirname, './public'), {index: ENV.debug ? 'debug.html' : 'index.html'});
-    router.get(({res, req,}) => {
-        console.log(req.url);
+    router.get(({res}) => {
+        res.send('不存在！');
     })
 };
